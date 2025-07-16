920638e0bbe9c9ec38755101e0ca9ce02df3f9dd
arbazali78600 <arbazali5592@gmail.com>
2025-07-16 13:40:04 +0530

    Simple calculator
    Design a simple calculator with basic arithmetic operations. Prompt the user to input two numbers and an operation choice. Perform the calculation and display the result.

---
+++ Simple calculator.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter first number: ")) num2 = float(input("Enter second number:"))
+
+choice = input("Choose Operator.. +, -, *, / :")
+
+if choice == "+":
+
+    print(num1 + num2)
+
+elif choice =="-":
+
+     print(num1 - num2)
+
+elif choice == "*":
+
+     print(num1 * num2)
+
+elif choice == "/":
+
+     print(num1 / num2)
+
+else:
+
+print("Invalid choice...")
